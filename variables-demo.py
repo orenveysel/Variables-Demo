a3290135c158f610f0e3febea56a53bd00755140
Veysel ÖREN <orenveysel@outlook.com>
2021-12-29 22:07:05 +0300

    Create variables-demo.py

---
+++ variables-demo.py
@@ -0,0 +1,17 @@
+musteriAd = "Ali"
+musteriSoyad = "Ağaoğlu"
+musteriAdSoyad = musteriAd + ' ' + musteriSoyad
+musteriCinsiyet = "Erkek"
+musteriTcNo = "98765432109"
+musteriDogumYili = 1930
+musteriAdres = "İstanbul Sancaktepe"
+musteriYas = 2020 - musteriDogumYili
+
+print(musteriYas)
+
+order1 = 110
+order2 = 1100.5
+order3 = 356.95
+total = order1 + order2 + order3
+
+print("Total:", total)
